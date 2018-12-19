b4e09296e8531c30a0b50951f12ab3c42852d574
Easter Bohanon <eastdabeast@email.arizona.edu>
2018-12-18 19:03:45 -0700

    Yo It is getting there

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 // Lets make our Variables that will set the functions of this site
 
-var topics = ["Thunder Cats", "Justice League", "Spongebob", "Ed Edd and Eddy", "CatDog", "Rugrats","Lizzy Mcguire", "Courage the Cowardly Dog", "South Park", "The Simpsons"];
+var topics = ["Thunder Cats", "Justice League", "Spongebob", "Ed Edd and Eddy", "CatDog", "Rugrats", "Lizzy Mcguire", "Courage the Cowardly Dog", "South Park", "The Simpsons"];
 
 
 var userInput = $("#input-val").val().trim();
@@ -19,27 +19,28 @@ function renderButtons() {
 
         var button = $("<button>");
         button.text(topics[i]);
-        button.attr("term", topics[i]);
+        button.attr("term", topics[i]).addClass("gif-buttons");
         $("#new-buttons-here").append(button);
         console.log(userInput);
         console.log(topics[i]);
-        
+
 
     }
 }
 
 $("#submit").on("click", function (event) {
-  
+
     event.preventDefault();
     userInput = $("#input-val").val().trim();
     // pushing those values into the topics array
     topics.push(userInput);
 
     console.log(userInput);
-    console.log(topics);
+    console.log("TOPICS", topics);
 
     // call our function
     renderButtons();
+    addingGifs();
 
 
 })
@@ -47,38 +48,48 @@ renderButtons();
 
 
 // when buttons are clicked will show gifs
-$("button").on("click", function (event){
+function addingGifs() {
+  $(".gif-buttons").on("click", function (event) {
     var cartoons = $(this).attr("term");
     console.log(this);
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cartoons + "&api_key=AZ8V9eq9gkTx4U3YUG0dXrTIHFK2EJki&limit=10";
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
-          var results = response.data;
-          console.log(response);
-          console.log(this);
+    }).then(function (response) {
+        var results = response.data;
+        console.log(response);
+        console.log(this);
 
-          for (var i = 0; i < results.length; i++) {
+        for (var i = 0; i < results.length; i++) {
             var gifDiv = $("<div>");
 
             var rating = results[i].rating;
-
+            // will show ratings
             var p = $("<p>").text("Rating: " + rating);
 
             var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height.url);
+            personImage.attr("src", results[i].images.fixed_height.url).addClass("actualGifs");
 
             gifDiv.prepend(p);
             gifDiv.prepend(personImage);
 
             $("#gifs-go-here").prepend(gifDiv);
-          }
+        }
 
-        });
+    });
+
+}); 
+}
+addingGifs();
+
+
+$(".actualGifs").on("click", function(){
+    console.log("clicking on gifs", this);
 });
+
 
 // Will pause and play image
 
-// will show ratings
+
 
